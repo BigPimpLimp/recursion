e46f1dd81abc1f656533480a71a9b4273c2f15f0
bigpimplimp <levi.jcecil@gmail.com>
2025-05-08 14:27:23 -0700

    add iterative fib function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 console.log('Sahhh dude')
 
 const fibs = function x(num) {
-  const arr = [];
-  if (n < 2) {
-    arr.push(n)
-    return arr;
+  let arr = [0];
+  if (num === 0) {
+    console.log(arr);
+    return;
   }
-  for (let i = 0; i < num; i++) {
-    
+  if (num === 1) {
+    arr.push(1);
+    console.log(arr);
+    return;
   }
+  if (num > 1) {
+    arr.push(1);
+    for (let i = 1; i < num; i++) {
+      let x = arr[arr.length - 1] + arr[arr.length - 2];
+      arr.push(x);
+    }
+    console.log(arr)
+    return;
+    // F = n-1 + n-2, where n > 1
+  }
+  console.log('Not valid input')
 }
+
+
+fibs(8)
