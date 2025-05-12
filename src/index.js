49998ba63a3dd1d2dc3e114f29a890c9f58b362e
BigPimpLimp <121207540+BigPimpLimp@users.noreply.github.com>
2025-05-12 09:26:32 -0400

    mergeSort

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 console.log('Sahhh dude')
+import './style.css';
+
 
 const fibs = function x(num) {
   let arr = [0];
@@ -24,5 +26,65 @@ const fibs = function x(num) {
   console.log('Not valid input')
 }
 
+const arr = [0, 1,];
 
-fibs(8)
+const fibsRec = function x(num) {
+  if (num == 0) {
+    let base = [0];
+    console.log(base);
+    return;
+  }
+  if (arr.length === num) {
+    console.log(arr);
+    return;
+  }
+  arr.push(arr[arr.length - 1] + arr[arr.length -2])
+  num - 1;
+  x(num)
+}
+
+
+const best = [4, 3, 2, 1, 0];
+
+const newArr = [];
+let hold;
+
+const mergeSort = function x(arr) {
+
+  if (arr.length > 1) {
+    let half = Math.ceil(arr.length / 2);
+    console.log(half)
+    let left = arr.slice(0, half);
+    let right = arr.slice(half)
+    console.log(arr)
+    console.log(left)
+    console.log(right)
+    if (half === 3) {
+      hold = left.splice(2);
+   
+    }
+    if (left.length === 1) {
+      if (left < right) {
+        let subSorted = [left[0], right[0]];
+        console.log(hold)
+        return subSorted
+      } else {
+        let subSorted = [right[0], left[0]];
+        console.log(hold)
+        return subSorted
+      }
+    }
+    const first = x(left) 
+    // const second = x(right);
+    console.log(first)
+    // console.log(second)
+    
+  }
+  if (arr.length === 1) {
+    console.log('lkajsdf')
+
+  }
+}
+
+mergeSort(best)
+
